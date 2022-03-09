08ec033894b2347d7da9f5cac3369e0dd76fd816
Yujen Chen <yujengogo@gmail.com>
2022-03-08 22:31:17 -0500

    adding condition for login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,32 @@
 
 async function handleLogin(event) {
   event.preventDefault()
+  const captchaResponse = grecaptcha.getResponse()
 
-  const username = document.getElementById('username').value
+  if (captchaResponse != "") {
+    const username = document.getElementById('username').value
 
-  const password = document.getElementById('password').value
+    const password = document.getElementById('password').value
 
-const response = grecaptcha.getResponse()
-console.log(response)
-  // if (username && password) {
-  //   // Send the e-mail and password to the server
-  //   const response = await fetch('/api/user/login', {
-  //     method: 'POST',
-  //     body: JSON.stringify({ username, password }),
-  //     headers: { 'Content-Type': 'application/json' },
-  //   });
+    if (username && password) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-  //   if (response.ok) {
-  //     document.location.replace('/');
-  //     console.log('your logged in')
-  //   } else {
-  //     alert('Failed to log in');
-  //   }
-  // }
+      if (response.ok) {
+        document.location.replace('/');
+        console.log('your logged in')
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  }
+  else {
+    alert('Please check reCAPTCHA');
+  }
 };
 
 document.querySelector('#loginForm').addEventListener('submit', handleLogin)
