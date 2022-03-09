d4a2a8ac60f1cab12fad243b25d85e4461482625
Yujen Chen <yujengogo@gmail.com>
2022-03-08 22:13:22 -0500

    recahptha check

--- public/js/login.js
+++ public/js/login.js
@@ -6,22 +6,23 @@ async function handleLogin(event) {
 
   const password = document.getElementById('password').value
 
+const response = grecaptcha.getResponse()
+console.log(response)
+  // if (username && password) {
+  //   // Send the e-mail and password to the server
+  //   const response = await fetch('/api/user/login', {
+  //     method: 'POST',
+  //     body: JSON.stringify({ username, password }),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   });
 
-  if (username && password) {
-    // Send the e-mail and password to the server
-    const response = await fetch('/api/user/login', {
-      method: 'POST',
-      body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-      console.log('your logged in')
-    } else {
-      alert('Failed to log in');
-    }
-  }
+  //   if (response.ok) {
+  //     document.location.replace('/');
+  //     console.log('your logged in')
+  //   } else {
+  //     alert('Failed to log in');
+  //   }
+  // }
 };
 
 document.querySelector('#loginForm').addEventListener('submit', handleLogin)
